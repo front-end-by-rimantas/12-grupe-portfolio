a685553240cf5c86a846a3445a23ac81503ab583
Rimantas <belauzas@yahoo.com>
2020-01-06 17:51:47 +0200

    Achievements blocks

--- js/data.js
+++ js/data.js
@@ -1,2 +1,31 @@
 "use strict";
 
+const achievements = [
+    {
+        icon: 'archive',
+        number: 245,
+        title: 'Total projects'
+    },
+    {
+    },
+    {
+        icon: 'users',
+        number: 288,
+        title: 'Experiences staff'
+    },
+    {
+        icon: 'smile-o',
+        number: 750,
+        title: 'Happy clients'
+    },
+    {
+        icon: 'smile-o',
+        number: 750,
+        title: 'Happy clients'
+    },
+    {
+        icon: 'smile-o',
+        number: 750,
+        title: 'Happy clients'
+    }
+]
--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,33 @@
 "use strict";
 
+function renderAchievements( data ) {
+    const maxBlocks = 4;
+    let createdBlocks = 0;
+    let HTML = '';
+
+    // sugeneruojame HTML
+    for ( let i=0; i<data.length; i++ ) {
+        if ( createdBlocks === maxBlocks ) {
+            break;
+        }
+        const block = data[i];
+
+        if ( block.icon &&
+             block.number &&
+             block.title ) {
+            HTML += `<div class="col-3 block">
+                        <i class="fa fa-${block.icon}"></i>
+                        <span>${block.number}</span>
+                        <h4>${block.title}</h4>
+                    </div>`;
+            
+            createdBlocks++;
+        }
+    }
+    console.log(createdBlocks);
+    
+
+    // istatome HTML i reikiama vieta
+    document.querySelector('#achievements > .row').innerHTML = HTML;
+    return;
+}
